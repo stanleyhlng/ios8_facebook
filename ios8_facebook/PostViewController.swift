db28434e5284c6f0b0372e9d4177e2d0f478d08b
stanleyn <stanleyhlng@yahoo.com.hk>
2014-09-05 00:09:23 -0700

    add post view

--- ios8_facebook/PostViewController.swift
+++ ios8_facebook/PostViewController.swift
@@ -1,33 +1,26 @@
 //
-//  FeedViewController.swift
+//  PostViewController.swift
 //  ios8_facebook
 //
-//  Created by Stanley Ng on 9/4/14.
+//  Created by Stanley Ng on 9/5/14.
 //  Copyright (c) 2014 Stanley Ng. All rights reserved.
 //
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class PostViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentImageView: UIImageView!
-    @IBOutlet weak var postButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-        println("FeedViewController - viewDidLoad")
+        println("PostViewController - viewDidLoad")
         
-        // config content view
         contentImageView.sizeToFit()
-        
-        // config scroll view
         scrollView.contentSize = CGSize(width: contentImageView.frame.width, height: contentImageView.frame.height + 56)
-        
-        // config post button
-        postButton.frame.origin.y = 228;
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +28,9 @@ class FeedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onBack(sender: UIButton) {
+        navigationController?.popViewControllerAnimated(true)
+    }
 
     /*
     // MARK: - Navigation
