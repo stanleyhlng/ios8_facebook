ebde9a55a5a0ec068a957af54c820e71c42b1f07
stanleyn <stanleyhlng@yahoo.com.hk>
2014-09-04 22:33:06 -0700

    setup custom view controllers

---
+++ ios8_facebook/MoreViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MoreViewController.swift
+//  ios8_facebook
+//
+//  Created by Stanley Ng on 9/4/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        println("MoreViewController - viewDidLoad")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
